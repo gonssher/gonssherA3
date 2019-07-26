d73d98fa360a28443c62e0b7ca833a9dcc667a4a
gonssher <52938571+gonssher@users.noreply.github.com>
2019-07-26 18:07:05 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,32 @@
+import { Book } from '../../app/bookInfo';
+
+export const MyBooks: Book[] = [
+    {
+        name: 'Treasure Island',
+        author: 'Enid Blyton',
+        genre: 'Fantasy',
+        publishedYear: '1942',
+        picture: 'assets/images/FantasyFiveOnAIsland.jpg'
+    },
+    {
+        name: 'A to Z Mysteries',
+        author: 'Ron Roy',
+        genre: 'Mystery',
+        publishedYear: '1998',
+        picture: 'assets/images/EmptyEnvelope.jpg'
+    },
+    {
+        name: 'TINKLE DOUBLE ',
+        author: 'RAJANI THINDIATH',
+        genre: 'Comic and Graphic Novel',
+        publishedYear: '2016',
+        picture: 'assets/images/TINKLEDOUBLEDIGEST161.jpg'
+    },
+    {
+        name: 'Silas Marner',
+        author: 'George Eliot',
+        genre: 'Fan-Fiction',
+        publishedYear: '1996',
+        picture: 'assets/images/SilasMarner.jpg'
+    }
+];
